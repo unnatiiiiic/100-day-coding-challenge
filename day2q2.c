638774cf025135410217ccd318d8b34223f8d822
unnatiiiiic <unnatic007@gmail.com>
2026-08-15 12:05:43 +0530

    Add comments to day2q2.c
    Added comments and sample test cases for circle area and circumference calculation.

--- day2q2.c
+++ day2q2.c
@@ -1,3 +1,18 @@
+//Q4: Write a program to calculate the area and circumference of a circle given its radius.
+
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+Area=153.94, Circumference=43.96
+
+Input 2:
+3
+Output 2:
+Area=28.27, Circumference=18.85
+
+*/
 #include <stdio.h>
 int main()
 {
