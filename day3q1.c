19314f485ae27270ad8dce4a0aaea146af2b1bb3
unnatiiiiic <unnatic007@gmail.com>
2026-08-15 12:06:20 +0530

    Add comments to day3q1.c
    Added a comment block for temperature conversion program.

--- day3q1.c
+++ day3q1.c
@@ -1,3 +1,18 @@
+//Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/
 #include<stdio.h>
 int main()
 {
