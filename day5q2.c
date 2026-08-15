5992c141088505a046eeef26532009aa61c1fedf
unnatiiiiic <unnatic007@gmail.com>
2026-08-15 12:11:03 +0530

    Add comments to day5q2.c

--- day5q2.c
+++ day5q2.c
@@ -1,3 +1,18 @@
+//Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+/*
+Sample Test Cases:
+Input 1:
+3661
+Output 1:
+1:1:1
+
+Input 2:
+7322
+Output 2:
+2:2:2
+
+*/
 #include <stdio.h>
 int main()
 {
