c32d3a07589a28c703dcbee9c8368c9ffadbaa2f
unnatiiiiic <unnatic007@gmail.com>
2026-08-15 12:12:18 +0530

    Add comments to day6q2.c

--- day6q2.c
+++ day6q2.c
@@ -1,3 +1,23 @@
+//Q12: Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+
+/*
+Sample Test Cases:
+Input 1:
+-5
+Output 1:
+Negative
+
+Input 2:
+0
+Output 2:
+Zero
+
+Input 3:
+10
+Output 3:
+Positive
+
+*/
 #include <stdio.h>
 int main()
 {
